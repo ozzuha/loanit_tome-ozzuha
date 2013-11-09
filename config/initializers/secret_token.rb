c05d3f99f887559599891f2bc73801db562256f7
Ozzuha <asska27@gmail.com>
2013-11-09 12:57:10 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Loanittome::Application.config.secret_key_base = '0787154bf9b9f9d392ac304cb2fa2118da69b91edbcd02da80220df2ebac4f6db84bde1a92c0ab2d504fb06f16f1c16e44f57366092aa42258bafd1f802c0f98'
